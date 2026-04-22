0f5a330189a258dde8bbd029f44ba7e425a26b4d
website3404-creator <website3404@gmail.com>
2026-04-23 05:31:52 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,19 @@
-// ហ្វាល sw.js សម្រាប់ដើរនៅ Background
+// ហ្វាល sw.js សម្រាប់ឱ្យ App ដើរនៅ Background
 self.addEventListener('push', function(event) {
-    const title = 'MATIN TOPUP: កម្មង់ថ្មី!';
     const options = {
         body: 'មានការកម្មង់ចូលថ្មី សូមពិនិត្យមើល!',
         icon: 'matin3-removebg-preview.png',
-        badge: 'matin3-removebg-preview.png',
-        vibrate: [500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170, 40],
+        vibrate: [500, 110, 500, 110, 450, 110, 200, 110, 170, 40],
         data: { url: './index.html' },
         tag: 'order-notification',
         renotify: true
     };
-
     event.waitUntil(
-        self.registration.showNotification(title, options)
+        self.registration.showNotification('MATIN TOPUP: កម្មង់ថ្មី!', options)
     );
 });
 
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    event.waitUntil(
-        clients.openWindow(event.notification.data.url)
-    );
+    event.waitUntil(clients.openWindow(event.notification.data.url));
 });
