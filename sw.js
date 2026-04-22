0a1bfa2d7d66a38298c10f05509ec92cb878a874
website3404-creator <website3404@gmail.com>
2026-04-23 06:05:48 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE_NAME = 'matin-admin-v1';
-
+// ហ្វាល sw.js
 self.addEventListener('install', (event) => {
     self.skipWaiting();
 });
@@ -8,24 +7,9 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
 });
 
-// មុខងារទាញទិន្នន័យនៅ Background (សម្រាប់ Android)
+// មុខងារនេះព្យាយាមទាញទិន្នន័យទោះបិទអែប (សម្រាប់ Android)
 self.addEventListener('periodicsync', (event) => {
     if (event.tag === 'check-new-orders') {
-        event.waitUntil(checkOrdersAndNotify());
+        event.waitUntil(fetchNewOrders());
     }
-});
-
-// មុខងារផ្ញើសារដែលរុញមកពី Server (Push Notification)
-self.addEventListener('push', (event) => {
-    const data = event.data ? event.data.text() : 'មានការកម្មង់ថ្មី!';
-    const options = {
-        body: data,
-        icon: 'matin3-removebg-preview.png',
-        vibrate: [500, 100, 500, 100, 500],
-        data: { url: './index.html' },
-        tag: 'order-notif',
-        renotify: true,
-        requireInteraction: true // សារនឹងនៅជាប់លើអេក្រង់ទាល់តែយើងចុចបិទ
-    };
-    event.waitUntil(self.registration.showNotification('MATIN TOPUP', options));
 });
